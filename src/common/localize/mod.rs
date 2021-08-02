245b9424605333fc3c9cc1bbd45d7fea0358361f
dylni <46035563+dylni@users.noreply.github.com>
2021-08-01 22:13:57 -0400

    Fix conditional warning

--- src/common/localize/mod.rs
+++ src/common/localize/mod.rs
@@ -4,6 +4,10 @@ use std::path::Path;
 #[cfg(any(target_os = "ios", target_os = "macos"))]
 mod macos;
 
+#[cfg_attr(
+    not(all(target_os = "ios", target_os = "macos")),
+    allow(unused_variables)
+)]
 pub(super) fn name(path: &Path) -> Option<OsString> {
     #[cfg(any(target_os = "ios", target_os = "macos"))]
     if let Some(path) = path.to_str() {
