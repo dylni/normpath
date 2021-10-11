da9b7b2a7c1981255a13162e2bd9f80d9b0bbe3e
dylni <46035563+dylni@users.noreply.github.com>
2021-10-10 23:09:04 -0400

    Temporarily fix rust-lang/rust#89658

---
+++ tests/edge_cases.rs
@@ -0,0 +1,21 @@
+mod common;
+
+#[cfg(windows)]
+#[test]
+fn test_edge_cases() {
+    use std::path::Path;
+
+    use normpath::BasePath;
+    use normpath::PathExt;
+
+    // https://github.com/dylni/normpath/pull/4#issuecomment-938596259
+    tj(r"X:\X:", r"ABC", r"X:\X:\ABC");
+    tj(r"\\?\X:\X:", r"ABC", r"\\?\X:\X:\ABC");
+
+    #[rustversion::attr(since(1.46.0), track_caller)]
+    fn tj(base: &str, path: &str, joined_path: &str) {
+        let joined_path = Path::new(joined_path);
+        assert_eq!(joined_path, BasePath::try_new(base).unwrap().join(path));
+        common::assert_eq(joined_path, joined_path.normalize_virtually());
+    }
+}
